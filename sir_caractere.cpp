d8eb04eafc9c75da5d40b031ec8053c1d53f7436
deliaogr <delia.ogrezeanu@gmail.com>
2020-07-31 20:35:18 +0300

    first commit

---
+++ sir_caractere.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    vector <string> sirCaractere;
+    string aux;
+    scanf("%[^\n]",aux);
+    cout<<sirCaractere[0];
+    return 0;
+}
